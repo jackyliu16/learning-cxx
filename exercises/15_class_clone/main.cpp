c3daa1e7b41c001826f7b4b45d05a32fba87c384
jackyliu16 <18922251299@163.com>
2024-12-20 17:37:05 +0100

    update 15

--- exercises/15_class_clone/main.cpp
+++ exercises/15_class_clone/main.cpp
@@ -9,18 +9,23 @@ class DynFibonacci {
     int cached;
 
 public:
-    // TODO: 实现动态设置容量的构造器
-    DynFibonacci(int capacity): cache(new ?), cached(?) {}
+    DynFibonacci(int capacity): cache(new size_t[capacity]{0, 1}), cached(2) {}
 
     // TODO: 实现复制构造器
-    DynFibonacci(DynFibonacci const &) = delete;
+    DynFibonacci(DynFibonacci const &original) : 
+      cache(new size_t[original.cached]),
+      cached(original.cached) {
+        for (int i=0; i < cached ; ++i) {
+          cache[i] = original.cache[i];
+        }
+      }
+    ;
 
     // TODO: 实现析构器，释放缓存空间
-    ~DynFibonacci();
+    ~DynFibonacci() { delete [] cache; };
 
-    // TODO: 实现正确的缓存优化斐波那契计算
     size_t get(int i) {
-        for (; false; ++cached) {
+        for (; cached <= i ; ++cached) {
             cache[cached] = cache[cached - 1] + cache[cached - 2];
         }
         return cache[i];
