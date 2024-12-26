ca295ab2ce429cd0731dd390935b479651571223
jackyliu16 <18922251299@163.com>
2024-12-27 05:20:36 +0800

    update 5: Covers situations where calculations cannot be completed in time

--- exercises/05_constexpr/main.cpp
+++ exercises/05_constexpr/main.cpp
@@ -12,15 +12,14 @@ constexpr unsigned long long fibonacci(int i) {
 }
 
 int main(int argc, char **argv) {
-constexpr auto FIB20 = fibonacci(20);
-ASSERT(FIB20 == 6765, "fibonacci(20) should be 6765");
-std::cout << "fibonacci(20) = " << FIB20 << std::endl;
+  constexpr auto FIB20 = fibonacci(20);
+  ASSERT(FIB20 == 6765, "fibonacci(20) should be 6765");
+  std::cout << "fibonacci(20) = " << FIB20 << std::endl;
 
-// TODO: 观察错误信息，修改一处，使代码编译运行
-// PS: 编译运行，但是不一定能算出结果……
-constexpr auto ANS_N = 90;
-auto ANS = fibonacci(ANS_N);
-std::cout << "fibonacci(" << ANS_N << ") = " << ANS << std::endl;
+  // NOTE: Covers situations where calculations cannot be completed in time
+  // constexpr auto ANS_N = 90;
+  // auto ANS = fibonacci(ANS_N);
+  // std::cout << "fibonacci(" << ANS_N << ") = " << ANS << std::endl;
 
-return 0;
+  return 0;
 }
